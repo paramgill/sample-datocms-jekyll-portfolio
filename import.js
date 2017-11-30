5be537924563f1d0c0d542a4cfa97c6fe720ca21
paramgill <paramgill@users.noreply.github.com>
2017-11-30 11:30:01 +1100

    updating the code to add events from whatson

--- import.js
+++ import.js
@@ -1,8 +1,8 @@
 require('babel-polyfill');
 require('dotenv').config();
-
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
 const fetch = require('node-fetch');
-//const jsonfile = require('jsonfile');
 const schedule = require('node-schedule');
 const toMarkdown = require('to-markdown');
 
@@ -11,40 +11,38 @@ var datoCmsKey = process.env.DATOCMS3794;
 const SiteClient = require('datocms-client').SiteClient;
 const client = new SiteClient(datoCmsKey);
 
+//client.items.all()
+//.then((models) => console.log(models));
+
 var file = './tmp/data.json';
-var whatsonid = "";
+var whatson_id = "";
 var title = "";
 var slug = "";
 var description = "";
 var excerpt = "";
 var image = "";
-
-//var j = schedule.scheduleJob('* * * * *', function () {
-fetch('https://whatson.cityofsydney.nsw.gov.au/api/v1/search/advanced?programs=art-and-about-sydney&date[]=2014-01-01&date[]=2017-12-31')
+var projectType = 18000;
+fetch('https://whatson.cityofsydney.nsw.gov.au/api/v1/search/advanced?programs=art-and-about-sydney&date[]=2014-01-01&date[]=2017-12-31&include_expired=true&per_page=100')
     .then(function (res) {
         return res.json();
     }).then(function (json) {
 
         data = json.data;
-
-        //findRecord(125995);
-        //showRecords();
-        //createItemsFromAPI(data);
-        awaitTest(data);
-        //client.items.update(itemId, resourceAttributes)
+        //deleteItemsFromAPI();     //delete all the events and related contents from Datocms
+        createItemsFromAPI(data);
 
     }).catch(function (err) {
         console.log(err);
     });
-//});
+
 
 // To retrieve records of a specific model
 function showRecords() {
     client.items.all({
-            'filter[type]': 'work'
-        })
+        'filter[type]': projectType
+    })
         .then(
-            (records) => console.log(records)
+        (records) => console.log(records)
         ).catch(function (err) {
             console.log(err);
         });
@@ -52,7 +50,6 @@ function showRecords() {
 
 // Return a specific item
 function findRecord(itemId) {
-    //client.items.find(itemId).then((item) => console.log(item.whatsonid));
     client.items.find(itemId).then((item) => {
         if ((item)) {
             console.log('True - Item exists');
@@ -64,52 +61,141 @@ function findRecord(itemId) {
     });
 }
 
+//update records in dato
+
 function createItemsFromAPI(data) {
-    for (var i in data) {
+    var ItemIds = [];
+    client.items.all({ 'filter[type]': projectType })                   
+        .then((records) => {
+            var cnt = 0;
+            for (var item in records) {
+                ItemIds.push(records[cnt].whatsonId);
+                cnt++;
+            };
+            (async(function testingAsyncAwait() {
+                //fethc all the items from datcms
+                for (var i in data) {
+                    id = data[i].id;
+                    if (ItemIds.includes(id)) {     //i
+                        console.log("Event ID " + id + " is already there in datocms");
+                    } else {
+                        title = data[i].title;
+                        slug = data[i].slug;
+                        eventid = data[i].id;
+                        description = toMarkdown(data[i].content);
+                        excerpt = data[i].excerpt;
+                        coverimage = data[i].thumbnails.thumbnail.src;
+                        cover_image = coverimage.replace("-150x150", "");
+                        await(
+                            client.uploadImage(cover_image)
+                                .then(image => {
+                                    console.log("Importing Event Id: " + id);
+                                    return client.items.create({
+                                        itemType: '18000',
+                                        whatson_id: eventid,
+                                        title: title,
+                                        slug: slug,
+                                        excerpt: excerpt,
+                                        content: description,
+                                        image_gallery: image
+                                    })
+                                }).catch(function (err) {
+                                    console.log(err);
+                                })
+                                .then(record => console.log()).catch(function (err) {
+                                    console.log(err);
+                                }))
+                    }
 
-        id = data[i].id;
-        title = data[i].title;
-        slug = data[i].slug;
-        description = toMarkdown(data[i].content);
-        excerpt = data[i].excerpt;
-
-        // Create a new record
-        client.items.create({
-            itemType: '16858',
-            whatsonid: id,
-            title: title,
-            slug: slug,
-            description: description,
-            excerpt: excerpt
+                }
+            }))();
         });
-    }
+
 }
 
-function awaitTest(data) {
-    (async() => {
+//this funtions is used to delete all the projects from datocms
+function deleteItemsFromAPI() {
 
-        for (var i in data) {
+    client.items.all({ 'filter[type]': projectType })
+        .then((records) => {
+            var cnt = 0;
+            for (var item in records) {
+                client.items.destroy(records[cnt].id);
+                console.log("Deleted : " + records[cnt].id + " WhatsonId : " + records[cnt].whatsonId);
+                cnt++;
+            };
+            console.log("All Deleted");
+        });
 
-            //console.log(data);
-
-            id = data[i].id;
-            title = data[i].title;
-            slug = data[i].slug;
-            description = toMarkdown(data[i].content);
-            excerpt = data[i].excerpt;
-            image = data[1].thumbnails.event_major.src
-
-            // Create a new record
-            // create a new Article
-            await client.items.create({
-                itemType: '16858',
-                whatsonid: id,
-                title: title,
-                slug: slug,
-                description: description,
-                excerpt: excerpt,
-                cover_image: (await client.uploadImage(image))
-            }).then((record) => console.log(record));
-        }
-    })();
 }
+
+//this funtions add the modular contents, but as it takes time and exits the loop before proeccsing the images so we are not going to use this
+//instead we will add excerpt and contents as notmal multiline firlds instead of modular content.
+
+
+/*
+
+function createItemsFromAPI(data) {
+    var ItemIds = [];
+    client.items.all({ 'filter[type]': '18000' })
+        .then((records) => {
+            var cnt = 0;
+            for (var item in records) {
+                console.log(records);
+                ItemIds.push(records[cnt].whatsonId);
+                cnt++;
+            };
+            (async(function testingAsyncAwait() {
+                //fethc all the items from datcms
+                for (var i in data) {
+                    id = data[i].id;
+                    console.log(ItemIds);
+                    console.log(data[i].id);
+                    if (ItemIds.includes(id)) {     //i
+                        console.log("event is already there in datocms");
+
+                    } else {
+                        title = data[i].title;
+                        slug = data[i].slug;
+                        eventid = data[i].id;
+                        description = toMarkdown(data[i].content);
+                        excerpt = data[i].excerpt;
+                        coverimage = data[i].thumbnails.thumbnail.src;
+                        cover_image = coverimage.replace("-150x150", "");
+                        console.log(cover_image);
+                        await(
+                            client.uploadImage(cover_image)
+                                .then((image) => {
+                                   client.items.create({
+                                        itemType: '17997',
+                                        copy: excerpt,
+                                    }).then(record => {
+                                        excerptid = record.id;
+                                         client.items.create({
+                                            itemType: '17998',
+                                            copy : toMarkdown(data[i].content)
+                                        }).then(record => {
+                                            contentid = record.id;
+                                            console.log("======++"+id+"====="+slug);
+                                            
+                                            return client.items.create({
+                                                itemType: '18000',
+                                                whatson_id: eventid,
+                                                title: title,
+                                                slug: slug,
+                                                modularContent: [excerptid,contentid],
+                                                image_gallery: image
+                                            })
+                                        })
+                                    })
+                                })
+                                .then(record => console.log()))
+                    }
+
+                }
+            }))();
+        });
+
+}
+
+*/
